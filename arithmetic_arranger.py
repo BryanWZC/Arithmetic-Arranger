73203c28a1f2bd7f7d656b6e522df4759d84e868
Bryan Wong <bryanwongzhicheng@gmail.com>
2021-02-05 18:19:37 -0500

    refactored code

--- arithmetic_arranger.py
+++ arithmetic_arranger.py
@@ -22,10 +22,6 @@ def arithmetic_arranger(problems, show_ans=False):
         # split the string into left operand, sign and right operand, and assign them accordingly
         split = re.split(r'([\+\-])', problem)
 
-        # raise exception if problem has less than or more than two operands
-        if len(split) != 3:
-            return 'Error: Missing second argument or wrong number of arguments.'
-
         left_operand = split[0];
         right_operand = split[2];
         sign = split[1];
@@ -42,29 +38,21 @@ def arithmetic_arranger(problems, show_ans=False):
         solve = int(left_operand) + int(right_operand) if sign == '+' else int(left_operand) - int(right_operand)
         solve = str(solve)
 
-        # find the lengths and difference for the operands
-        left_len = len(left_operand)
-        right_len = len(right_operand)
-        diff = abs(left_len - right_len)
+        # find the max string length
+        max_len = max([len(left_operand), len(right_operand)])
         
-        # perform string manipulations based on whichever operand is longer
-        if(left_len > right_len):
-            left_str += left_operand.rjust(2 + left_len)
-            right_str += sign + ' ' + right_operand.rjust(left_len)
-            dash_str += ''.rjust(left_len + 2, '-')
-            solve_str += solve.rjust(left_len + 2)
-        else:
-            left_str += left_operand.rjust(2 + right_len)
-            right_str += sign + ' ' + right_operand
-            dash_str += ''.rjust(right_len + 2, '-')
-            solve_str += solve.rjust(right_len + 2)
+        # perform string manipulations based on max string length
+        right_str += sign + ' ' + right_operand.rjust(max_len)
+        left_str += left_operand.rjust(max_len + 2)
+        dash_str += ''.rjust(max_len + 2, '-')
+        solve_str += solve.rjust(max_len + 2)
         
         # add a padding for the next operation if not the last index
         if(index < len(problems) - 1):
-            left_str += '    '
-            right_str += '    '
-            dash_str += '    '
-            solve_str += '    '
+            left_str += ''.rjust(4)
+            right_str += ''.rjust(4)
+            dash_str += ''.rjust(4)
+            solve_str += ''.rjust(4)
 
     # if option to show answer is True, show answer
     if show_ans:
